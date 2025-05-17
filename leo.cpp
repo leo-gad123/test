ec051b1f72f1ac65ef66d1990aab98bfcd889bc0
leo-gad123 <hakizimanaleogad@gmail.com>
2025-05-17 09:10:06 -0700

    this my c++

---
+++ leo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for (int i=0;i<5;i++)
+    cout<<"1"<<endl;
+    for(int a=1;a<5;a++){
+        for(int b=1;b<5;b++)
+            cout<<" ";
+            cout<<"1"<<endl;
+    
+    }
+    for (int i=0;i<5;i++)
+    cout<<"1"<<endl;
+}
